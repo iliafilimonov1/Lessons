b498b17fbe5c651d257b084eac3896ba5466bca9
Ilia Filimonov <9991571155i@gmail.com>
2024-10-23 18:56:13 +0500

    Добавит логику редактирования товара

--- pd-212/src/pages/Admin.jsx
+++ pd-212/src/pages/Admin.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import Table from "../components/ui/Table/Table";
 import useItems from "../store/useItems";
 import useDisclosure from "../hooks/useDisclosure";
 import { Drawer } from "../components/ui/Drawer/Drawer";
-import { useState } from "react";
+import useForm from "../hooks/useForm";
+import Alert from "../components/ui/Alert/Alert";
 
 const Admin = () => {
   // Достаем данные из стора.
-  const { items } = useItems();
+  const { items, editItem } = useItems();
 
   // Drawer для чтения информации
   const itemDrawer = useDisclosure();
 
-  // Состояние для передачи данных в Drawer
-  const [itemData, setItemData] = useState(null);
+  // Стейт для скрытия/показа компонента Alert
+  const [alertData, setAlertData] = useState({
+    title: "",
+    subtitle: "",
+    variant: "neutral",
+    isOpen: false,
+  });
+
+  // Данные полученные при двойном клике на строку таблицы
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  // Стейт для переключения режима редактирования
+  const [isEditing, setIsEditing] = useState(false);
+
+  // Обработка данных формы.
+  const { formValues, formErrors, resetForm, handleChange } = useForm(selectedValue);
 
   /**
    * Обрабатывает двойной клик по строке таблицы.
@@ -21,8 +37,38 @@ const Admin = () => {
    * @returns {void}
    */
   const handleRowDoubleClick = (rowData) => {
-    setItemData(rowData);
+    setSelectedValue(rowData);
+    setIsEditing(false);
     itemDrawer?.onOpen();
+  };
+
+  /**
+   * Обработка отправки формы.
+   * Если товар выбран, редактируем его, иначе добавляем новый товар.
+   *
+   * @param {Event} event - Событие отправки формы.
+   * @returns {void}
+   */
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (selectedValue) {
+      editItem(selectedValue?.id, { ...selectedValue, ...formValues });
+    }
+
+    // Уведомляем пользователя об изменениях
+    setAlertData({
+      title: "Редактирование товара.",
+      subtitle: "Товар был успешно отредактирован.",
+      variant: "neutral",
+      isOpen: true,
+    });
+
+    // Закрываем Drawer
+    itemDrawer?.onClose();
+
+    // Сбрасываем состояние формы
+    resetForm();
   };
 
   return (
@@ -42,10 +88,115 @@ const Admin = () => {
       <Drawer
         isOpen={itemDrawer?.isOpen}
         onClose={itemDrawer?.onClose}
-        title="Чтение данных по товару"
+        title="Read product information"
       >
-        <div className="w-full max-w-xs">{itemData?.name}</div>
+        <div className="w-full max-w-xs">
+          <form onSubmit={handleFormSubmit}>
+            <div className="mb-4">
+              <label
+                className="block text-sm mb-2 font-medium text-gray-700"
+                htmlFor="name"
+              >
+                Product name
+              </label>
+              <input
+                className="read-only:border-gray-200 read-only:text-gray-400 read-only:bg-gray-100 read-only:cursor-not-allowed appearance-none h-10 px-4 block border w-full border-gray-300 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                name="name"
+                type="text"
+                data-validate="text"
+                required
+                defaultValue={formValues?.name || selectedValue?.name}
+                onChange={handleChange}
+                placeholder="Enter name"
+                readOnly={!isEditing}
+              />
+              <span className="text-rose-500 text-xs italic mt-2">
+                {formErrors?.name}
+              </span>
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="category"
+                className="block text-sm mb-2 font-medium text-gray-700"
+              >
+                Product category
+              </label>
+              <select
+                name="category"
+                autoComplete="off"
+                required
+                onChange={handleChange}
+                disabled={!isEditing}
+                className="h-10 px-4 block bg-white border-gray-300 w-full border rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm disabled:border-gray-200 disabled:text-gray-400 disabled:bg-gray-100 disabled:cursor-not-allowed appearance-none"
+              >
+                <option value="chair">Chair</option>
+                <option value="bed">Bed</option>
+                <option value="bench">Bench</option>
+                <option value="children's furniture">
+                  Children's Furniture
+                </option>
+                <option value="sofa beds">Sofa Beds</option>
+              </select>
+            </div>
+
+            <div className="mb-8">
+              <label
+                className="block text-sm mb-2 font-medium text-gray-700"
+                htmlFor="price"
+              >
+                Product price
+              </label>
+              <input
+                readOnly={!isEditing}
+                className="w-32 read-only:border-gray-200 read-only:text-gray-400 read-only:bg-gray-100 read-only:cursor-not-allowed appearance-none px-4 h-10 border block border-gray-300 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                name="price"
+                type="text"
+                data-validate="text"
+                required
+                defaultValue={formValues?.price || selectedValue?.price}
+                onChange={handleChange}
+                placeholder="Enter price"
+              />
+              <span className="text-rose-500 text-xs italic mt-2">
+                {formErrors?.price}
+              </span>
+            </div>
+
+            <div className="mb-4 flex justify-end">
+              {!isEditing && selectedValue && (
+                <>
+                  <button className="flex-1 border-2 border-rose-500 bg-rose-500 text-white font-medium py-2 px-4 rounded">
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => setIsEditing(true)}
+                    className="flex-1 ml-2 border-2 border-indigo-500 bg-indigo-500 text-white font-medium py-2 px-4 rounded"
+                  >
+                    Edit
+                  </button>
+                </>
+              )}
+              {isEditing && (
+                <button className="w-full border-2 border-indigo-500 bg-indigo-500 text-white font-medium py-2 px-4 rounded">
+                  Save changes
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
       </Drawer>
+
+      <Alert
+        title={alertData?.title}
+        subtitle={alertData?.subtitle}
+        variant={alertData?.variant}
+        isOpen={alertData?.isOpen}
+        onClose={() => {
+          setAlertData((prevAlertData) => ({
+            isOpen: !prevAlertData.isOpen,
+          }));
+        }}
+      />
     </section>
   );
 };
--- pd-212/src/store/useItems.js
+++ pd-212/src/store/useItems.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const useItems = create((set) => {
+const useItems = create((set, get) => {
   // Список товаров.
   const items = [];
 
@@ -8,6 +8,7 @@ const useItems = create((set) => {
   const fetchItems = async () => {
     try {
       const response = await fetch(`http://localhost:3000/items`);
+
       const data = await response?.json();
 
       console.log("Fetched items:", data);
@@ -18,9 +19,48 @@ const useItems = create((set) => {
     }
   };
 
+  /**
+   * Функция для обновления товара по id.
+   *
+   * @param {number} id - id товара, который необходимо обновить.
+   * @param {Object} updatedItem - Обновленные данные товара.
+   * @returns {Promise<void>} - Промис.
+   * @throws {Error} - Выбрасывает ошибку, если HTTP-запрос завершился неудачей.
+   */
+  const editItem = async (id, updatedItem) => {
+    console.log(updatedItem);
+    try {
+      const response = await fetch(`http://localhost:3000/items/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON?.stringify(updatedItem),
+      });
+
+      if (!response?.ok) {
+        throw new Error(`HTTP error! Status: ${response?.status}`);
+      }
+
+      const data = await response?.json();
+
+      const updatedItems = get()?.items?.map((item) => {
+        if (item?.id === id) {
+          return { ...item, ...data };
+        }
+        return item;
+      });
+
+      set({ items: updatedItems });
+    } catch (error) {
+      console.error("Error updating product:", error);
+    }
+  };
+
   return {
     items,
     fetchItems,
+    editItem
   };
 });
 
